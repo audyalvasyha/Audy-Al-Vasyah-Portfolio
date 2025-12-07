28922698ef136a2dbea32bb6498c04064c5df672
Audy Al Vasyah <audialfasha@gmail.com>
2025-12-07 12:56:06 +0000

    Try fixing this error: `Console Error: Firebase admin initialization err

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,9 +2,9 @@ import admin from 'firebase-admin';
 import { getDatabase } from 'firebase-admin/database';
 
 const serviceAccount = {
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  project_id: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
 };
 
 let adminDb: admin.database.Database;
